f42b9f1e23ec8218b3f6c0e9ec4a209e9503780a
coler706 <coler706@gmail.com>
2016-05-13 12:51:07 -0700

    Update World.java

--- src/cm/cubestruct/world/World.java
+++ src/cm/cubestruct/world/World.java
@@ -3,6 +3,7 @@ package cm.cubestruct.world;
 public class World {
 	public Chunk[][] chunks=new Chunk[16][16];
 	public Generator gen=new SuperFlatGen();
+	public static final int WORLD_HEIGHT=256;
 	public World(Generator gen2){
 		gen=gen2;
 	}
