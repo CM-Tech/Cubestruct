fa78976117140183f272025c1c79eced03a182da
modderme123 <modderme123@users.noreply.github.com>
2016-05-13 15:24:32 -0700

    More efficient hue conversion

--- resources/shaders/rainbow.fs
+++ resources/shaders/rainbow.fs
@@ -5,35 +5,32 @@ out vec4 fragColor;
 
 uniform sampler2D texture_sampler;
 
-vec4 hsv_to_rgb(float h, float s, float v, float a)
+vec4 hue_to_rgb(float h)
 {
-	float c = v * s;
-	h = mod((h * 6.0), 6.0);
-	float x = c * (1.0 - abs(mod(h, 2.0) - 1.0));
+	h = mod((h * 6.), 6.);
+	float x = (1. - abs(mod(h, 2.) - 1.));
 	vec4 color;
 
-	if (0.0 <= h && h < 1.0) {
-		color = vec4(c, x, 0.0, a);
-	} else if (1.0 <= h && h < 2.0) {
-		color = vec4(x, c, 0.0, a);
-	} else if (2.0 <= h && h < 3.0) {
-		color = vec4(0.0, c, x, a);
-	} else if (3.0 <= h && h < 4.0) {
-		color = vec4(0.0, x, c, a);
-	} else if (4.0 <= h && h < 5.0) {
-		color = vec4(x, 0.0, c, a);
-	} else if (5.0 <= h && h < 6.0) {
-		color = vec4(c, 0.0, x, a);
+	if (0. <= h && h < 1.) {
+		color = vec4(1., x, 0., 1.);
+	} else if (1. <= h && h < 2.) {
+		color = vec4(x, 1., 0., 1.);
+	} else if (2. <= h && h < 3.) {
+		color = vec4(0., 1., x, 1.);
+	} else if (3. <= h && h < 4.) {
+		color = vec4(0., x, 1., 1.);
+	} else if (4. <= h && h < 5.) {
+		color = vec4(x, 0., 1., 1.);
+	} else if (5. <= h && h < 6.) {
+		color = vec4(1., 0., x, 1.);
 	} else {
-		color = vec4(0.0, 0.0, 0.0, a);
+		color = vec4(0., 0., 0., 1.);
 	}
-
-	color.rgb += v - c;
 
 	return color;
 }
 
 void main()
 {
-    fragColor = texture(texture_sampler, outTexCoord)+hsv_to_rgb(outTexCoord.x, 1., 1., 1.);
+    fragColor = texture(texture_sampler, outTexCoord)+hue_to_rgb(outTexCoord.x-outTexCoord.y);
 }
